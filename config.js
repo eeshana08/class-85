80bc4b830c23a72354589474bc57d27db7917624
eeshana08 <104460496+eeshana08@users.noreply.github.com>
2023-06-19 17:28:46 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBOW_GSZpCtnCk94on0JrmevbklHQ7QlVE",
+  authDomain: "story-telling-app-46895.firebaseapp.com",
+  databaseURL: "https://story-telling-app-46895-default-rtdb.firebaseio.com",
+  projectId: "story-telling-app-46895",
+  storageBucket: "story-telling-app-46895.appspot.com",
+  messagingSenderId: "730730074451",
+  appId: "1:730730074451:web:94972d7a05a4adacbe51a6"
+};
